cf7f93b3d451067804a6a6211643d72543b7de1a
patpatmw <patmziya@gmail.com>
2022-11-29 17:20:50 -0800

    random number generation

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,7 +5,7 @@ number = random.randint(-10, 10)
 
 if number > 0:
         print(f"{number:d} is positive")
-    elif number == 0:
-            print(f"{number:d} is zero")
-        else:
-                print(f"{number:d} is negative")
+elif number == 0:
+        print(f"{number:d} is zero")
+else:
+        print(f"{number:d} is negative")
