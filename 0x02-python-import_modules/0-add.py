8a0f21212e1f5ff5499de389117a902032ac6ef4
patpatmw <patmziya@gmail.com>
2022-12-01 18:31:31 -0800

    edits

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,8 +4,6 @@ if __name__ == "__main__":
 
     from add_0 import add
 
-
-
     a = 1
 
     b = 2
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,8 +4,6 @@ if __name__ == "__main__":
 
     from calculator_1 import add, sub, mul, div
 
-
-
     a = 10
 
     b = 5
