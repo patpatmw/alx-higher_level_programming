7ef5e9c7a7e178aa744fe60c6629bed02d63a6d3
patpatmw <patmziya@gmail.com>
2023-03-24 18:30:25 -0700

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,6 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          port=3306,
@@ -15,7 +14,9 @@ if __name__ == "__main__":
     cur = db.cursor()
     # HERE I have to know SQL to grab all states in my database
     cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
+        "SELECT * FROM states
+        WHERE name LIKE BINARY 'N%'
+        ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
